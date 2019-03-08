23410623ddb18af25e18644605b34fc6f137a9df
JJ <tangjun@zuzuche.com>
2019-03-08 14:32:46 +0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const Framework = require('./framework');
 const app = new Framework();
 
+app.use(async (ctx, next) => {
+  console.log(123);
+  await next();
+});
 app.use((ctx) => {
   ctx.body = 123;
 });
--- framework.js
+++ framework.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const compose = require('koa-compose');
 
 class Framework {
   constructor() {
@@ -17,27 +16,16 @@ class Framework {
   }
 
   createContext(req, res) {
-    const context = new Proxy(this.context, {
-      get: function (target, key, receiver) {
-        return Reflect.get(target, key, receiver);
-      },
-      set: function (target, key, value, receiver) {
-        if(key == 'body' && value){
-          value = value.toString();
-          res.end(value);
-        }
-        return Reflect.set(target, key, value, receiver);
-      }
-    });
+    const context = Object.create(this.context);
     context.req = req;
     context.res = res;
-    
+
 
     return context;
   }
 
   callback() {
-    // const fn = compose(this.middleware);
+    const fn = compose(this.middleware);
 
     // if (!this.listeners('error').length) this.on('error', this.onerror);
 
@@ -45,9 +33,94 @@ class Framework {
       res.statusCode = 404;
       const ctx = this.createContext(req, res);
       // onFinished(res, ctx.onerror);
-      this.middleware[0](ctx)
+      fn(ctx).then(() => respond(ctx)).catch(ctx.onerror);
     };
   }
+}
+
+function compose(middleware) {
+  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+  for (const fn of middleware) {
+    if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+  }
+
+  /**
+   * @param {Object} context
+   * @return {Promise}
+   * @api public
+   */
+
+  return function (context, next) {
+    // last called middleware #
+    let index = -1
+    return dispatch(0)
+    function dispatch(i) {
+      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+      index = i
+      let fn = middleware[i]
+      if (i === middleware.length) fn = next
+      if (!fn) return Promise.resolve()
+      try {
+        return Promise.resolve(fn(context, function next() {
+          return dispatch(i + 1)
+        }))
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    }
+  }
+}
+
+
+/**
+ * Response helper.
+ */
+
+function respond(ctx) {
+  // allow bypassing koa
+  if (false === ctx.respond) return;
+
+  const res = ctx.res;
+  if (!ctx.writable) return;
+
+  let body = ctx.body;
+  const code = ctx.status;
+
+  // ignore body
+  if (statuses.empty[code]) {
+    // strip headers
+    ctx.body = null;
+    return res.end();
+  }
+
+  if ('HEAD' == ctx.method) {
+    if (!res.headersSent && isJSON(body)) {
+      ctx.length = Buffer.byteLength(JSON.stringify(body));
+    }
+    return res.end();
+  }
+
+  // status body
+  if (null == body) {
+    body = ctx.message || String(code);
+    if (!res.headersSent) {
+      ctx.type = 'text';
+      ctx.length = Buffer.byteLength(body);
+    }
+    return res.end(body);
+  }
+
+  // responses
+  if (Buffer.isBuffer(body)) return res.end(body);
+  if ('string' == typeof body) return res.end(body);
+  if (body instanceof Stream) return body.pipe(res);
+
+  // body: json
+  body = JSON.stringify(body);
+  if (!res.headersSent) {
+    ctx.length = Buffer.byteLength(body);
+  }
+  res.end(body);
 }
 
 module.exports = Framework;
