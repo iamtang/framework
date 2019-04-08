480ae25a38fcf5e1be63d7f076bcdf541472283b
JJ <tangjun@zuzuche.com>
2019-04-08 10:13:07 +0800

    test

--- framework.js
+++ framework.js
@@ -35,7 +35,6 @@ class Framework {
   }
 }
 
-
 function compose(middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
   for (const fn of middleware) {
